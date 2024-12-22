8e914216d3685e16822133adb6d5365ee797dfe9
swaroop-gaikwad <swaroopgaikwad3586@gmail.com>
2024-12-22 14:35:12 +0530

    Deleted data

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
 
-print ("Whats upp Guys!!!!")
-
-print ("Added newer version")
-
-print ("Added new changes for POLL SCM")
-
-print ("ankit")
-
-print("leo_Messi")
-
-print("swaroop")
-
-print("checking difference")
+print ("Deleted data")
